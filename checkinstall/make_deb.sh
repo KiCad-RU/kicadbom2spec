984f2c8ed0dc9183554971a9998e891ac8b6c8dc
Baranovskiy Konstantin <baranovskiykonstantin@gmail.com>
2015-05-08 17:18:28 +0300

    Linux installer: finished work on debian package creation.

---
+++ checkinstall/make_deb.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+VERSION=$(../kicadbom2spec.pyw -v)
+sudo checkinstall -D \
+    --install=no \
+    --pkgname=kicadbom2spec \
+    --pkgversion=$VERSION \
+    --pkgarch=all \
+    --pkgrelease=1 \
+    --pkglicense=GPLv3 \
+    --pkggroup=utils \
+    --pkgsource=http://launchpad.net/kicadbom2spec \
+    --pakdir=../../ \
+    --maintainer=baranovskiykonstantin@gmail.com \
+    --requires="python2.7",'"python-wxgtk2.8|python-wxgtk3.0"',"python-odf" \
+    --nodoc \
+    --backup=no \
+    ./install.sh
